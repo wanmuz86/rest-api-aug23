60e88a03cde37a3aba8d9905359a283ec97f6e45
Wan Muzaffar <wanmuz86@gmail.com>
2023-08-25 09:21:47 +0800

    add correct config

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use App\Models\Review;
+use App\Models\Place;
 
 class ReviewController extends Controller
 {
     //
+
+    public function store(Request $request,$id){
+
+        try {
+        $user = JWTAuth::parseToken()->authenticate();
+        $userId = $user->id;
+
+        $place = Place::with('reviews')->where('id',$id)->first();
+      
+        $place["avg_rating"]= ($place["avg_rating"]* count($place["reviews"]) + $request->rating) / ( count($place["reviews"]) +1);
+       
+        Review::create([
+            "user_id"=>$userId,
+            "place_id"=>$id,
+            "rating"=>$request->rating,
+            "comments"=>$request->comments
+        ]);
+
+        $place->save();
+        return response()->json(["message"=>"Review successfully added",
+        "success"=>true
+    ]);
+}
+catch (\Exception $e) {
+    return response()->json(['error' => 'Add review failed ' . $e], 500);
+}
+
+        
+
+      
+
+
+
+
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -10,7 +10,7 @@ class Review extends Model
     use HasFactory;
     protected $fillable = [
         'rating',
-        'review',
+        'comments',
         'user_id',
         'place_id'
     ];
